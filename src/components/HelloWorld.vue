640796424cdf1bc67510636bf117f0e6ee0965a3
ronen-e <alter.ronen@gmail.com>
2019-12-16 22:10:28 +0200

    Add Vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,64 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+	<main>
+		<section>
+			<header>IP Generator</header>
+			<article>
+				<p>
+					<input v-model="query" placeholder="IP or Domain name" type="text" autocomplete="off" />
+				</p>
+				<button @click="getData()">Get GEO data</button>
+			</article>
+			<article v-if="result">
+				<p>Here is your GEO data</p>
+				<pre>{{result}}</pre>
+			</article>
+		</section>
+	</main>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+	export default {
+		name: "HelloWorld",
+
+		data() {
+			return {
+				query: '',
+				result: ''
+			};
+		},
+		methods: {
+			getData() {
+				fetch("http://ip-api.com/json/" + this.query)
+					.then(res => res.json())
+					.then(this.update)
+					.catch(this.onError);
+			},
+			update(data) {
+				this.result = JSON.stringify(data, null, 2);
+			},
+			onError(e) {
+				alert(e);
+			}
+		}
+	};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+	section {
+		padding: 0 80px;
+	}
+	article {
+		padding: 10px;
+		margin: 20px;
+		border: 1px solid gainsboro;
+	}
+
+	button {
+		background-color: rgb(83, 170, 83);
+		padding: 10px;
+		border-radius: 5px;
+		color: white;
+		cursor: pointer;
+	}
 </style>
